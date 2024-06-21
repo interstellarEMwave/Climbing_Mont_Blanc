2adcbc347679020d060ee145a9720da5f82fc578
interstellarEMwave <ingemar@live.no>
2024-06-21 22:02:27 +0200

    New small test image, with a circle

--- make_test_image.py
+++ make_test_image.py
@@ -1,10 +1,20 @@
 import numpy as np
 import imageio
+import random
 
-def main():
+def make_test_image():
     
     image = np.zeros((81,81), dtype=np.uint8)
-    image[40][40] = 255
     
-    imageio.v2.imwrite("test_image_one_channel.png", image) 
-main()
+    #for i in range(81):
+    #    for j in range(81):
+    #        image[i][j] = np.random.randint(0, 255)
+
+    rad = 15
+    for i in range(81):
+        for j in range(81):
+            if((40-i)**2 + (40-j)**2 < rad**2):
+                image[i][j] = 255
+
+    imageio.v2.imwrite("test_image_one_channel.png", image)
+    return image
