a8a1bb4e436155e5ce03dff8b5b647e338606f64
interstellarEMwave <ingemar@live.no>
2024-06-21 21:57:39 +0200

    Made it work

--- blur_correctness.py
+++ blur_correctness.py
@@ -6,7 +6,7 @@ def main():
     
     #tempImage = imageio.v2.imread("./test_image_one_channel.png")
     tempImage = make_test_image()
-    
+
     kernels = [2, 3, 5, 8]
     iterations = 5
        
@@ -27,6 +27,10 @@ def main():
     for i in range(len(outImages)):
         ax[i+1].imshow(outImages[i])
 
+
+
+
+
 def makeSummedAreaTable(imageIn):
     imageOut = np.zeros(imageIn.shape, dtype = np.float32)
     for i in range(len(imageIn)):
@@ -41,6 +45,24 @@ def makeSummedAreaTable(imageIn):
     
     return imageOut
 
+def makeSeparation(imageIn, kernel):
+    imageHeight = len(imageIn)
+    imageWidth = len(imageIn[0])
+    kernelWidth = kernel*2 + 1
+    out = []
+
+    for i in range(kernelWidth):
+        out.append([])
+        for j in range(kernelWidth):
+            out[i].append(np.zeros(((imageHeight+4*kernel)//kernelWidth, (imageWidth+4*kernel)//kernelWidth), dtype = np.float32))
+
+    for i in range(imageHeight + kernelWidth - 1):
+        for j in range(imageWidth + kernelWidth - 1):
+            out[i%kernelWidth][j%kernelWidth][i//kernelWidth][j//kernelWidth] = imageIn[min(i, imageHeight - 1)][min(j, imageWidth-1)]
+
+    return out
+
+
 def blurSummedAreaTable(imageBuffers, kernel, iterations):
     print("-"*100)
     print("blurring with kernel:", kernel)
@@ -48,46 +70,52 @@ def blurSummedAreaTable(imageBuffers, kernel, iterations):
 
     bufferY = len(imageBuffers[0])
     bufferX = len(imageBuffers[0][0])
-    
+
     cursorIn = 0
     cursorOut = 1
     for it in range(iterations):
-        imageBuffers[cursorIn] = makeSummedAreaTable(imageBuffers[cursorIn]) 
+        imageBuffers[cursorIn] = makeSummedAreaTable(imageBuffers[cursorIn])
+        image = makeSeparation(imageBuffers[cursorIn], kernel)
+
         for i in range(bufferY):
             for j in range(bufferX):
-                imageBuffers[cursorOut][i][j] = blurStep(imageBuffers[cursorIn], kernel, i, j)
-        
+                imageBuffers[cursorOut][i][j] = blurStep(image, bufferY, bufferX, kernel, i, j)
+
         cursorIn = cursorOut
         cursorOut = abs(cursorIn-1)
-    
+   
     outImage = np.zeros(imageBuffers[0].shape, dtype=np.uint8)
     for i in range(bufferY):
         for j in range(bufferX):
             outImage[i][j] = np.uint8(imageBuffers[cursorIn][i][j])
-    
+
     return outImage
 
 
-
-def blurStep(bufferIn, kernel, y, x):
+def blurStep(image, outImageHeight, outImageWidth, kernel, y, x):
+    kernelWidth = 2*kernel + 1
     ymin = y - kernel - 1
     xmin = x - kernel - 1
-    ymax = min(y+kernel, len(bufferIn)-1)
-    xmax = min(x+kernel, len(bufferIn[0])-1)
+    y1 = (y+kernel)%kernelWidth
+    x1 = (x+kernel)%kernelWidth
+    y2 = (y+kernel)//kernelWidth
+    x2 = (x+kernel)//kernelWidth
+
     
-    dividend = bufferIn[ymax][xmax]
+    dividend = image[y1][x1][y2][x2]
 
     if not (ymin < 0 or xmin < 0):
-        dividend += bufferIn[ymin][xmin] 
+        dividend += image[y1][x1][y2 - 1][x2 - 1]
     if not (xmin < 0):
-        dividend -= bufferIn[ymax][xmin] 
+        dividend -= image[y1][x1][y2][x2 - 1] 
     if not (ymin < 0):
-        dividend -= bufferIn[ymin][xmax];
+        dividend -= image[y1][x1][y2 - 1][x2] 
 
-    divisor = min(2*kernel+1, kernel + 1 + y, len(bufferIn) - y + kernel) * min(2*kernel+1, kernel + 1 + x, len(bufferIn) - x + kernel);
-  
+    divisor = min(kernelWidth, kernel + 1 + y, outImageHeight - y + kernel) * min(kernelWidth, kernel + 1 + x, outImageWidth - x + kernel);
+
+    
     return dividend/float(divisor)  
-
+  
 
 
 main()
